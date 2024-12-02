cfb4c0dcde7cd7a23d6c355fd4c01f110cd2f28b
Prabh <Prabhjeetaluna23@gmail.com>
2024-12-01 20:40:43 -0800

    animations
    Added in some movement animations, and worked on parts of movement script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const jump_power = -300.0
 
+var gravity = 900
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +13,30 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_power
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	handle_movement_animation(direction)
+	
+func handle_movement_animation(direction):
+	if is_on_floor():
+		if !velocity:
+			animated_sprite.play("idle")
+		if velocity:
+			animated_sprite.play("run")
+			toggle_flip_sprite(direction)
+		
+func toggle_flip_sprite(direction):
+	if direction == 1:
+		animated_sprite.flip_h = false
+	if direction == -1:
+		animated_sprite.flip_h = true
