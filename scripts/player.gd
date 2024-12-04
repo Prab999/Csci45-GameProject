012b8be3c3a7eef1df46b2f5f6abedfbbe4dc24c
Prabh <Prabhjeetaluna23@gmail.com>
2024-12-03 16:20:10 -0800

    New player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+var is_crouching = false
+
 const SPEED = 130.0
 const jump_power = -300.0
 
@@ -11,11 +13,12 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_power
-
+		animated_sprite.play("jump")
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
@@ -23,22 +26,53 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if Input.is_action_just_pressed("crouch"):
+		crouch()
+	elif Input.is_action_just_released("crouch"):
+		stand()
 
 	move_and_slide()
 	handle_movement_animation(direction)
 	
+
 func handle_movement_animation(direction):
 	if is_on_floor():
 		if !velocity:
-			animated_sprite.play("idle")
+			if is_crouching:
+				animated_sprite.play("crouch")
+			else:
+				animated_sprite.play("idle")
 		if velocity:
-			animated_sprite.play("run")
+			if is_crouching:
+				animated_sprite.play("crouch_walk")
+			else:
+				animated_sprite.play("run")
 			toggle_flip_sprite(direction)
 	elif !is_on_floor():
-		animated_sprite.play("fall")
+		if is_crouching == false:
+			if velocity.y < 0:
+				animated_sprite.play("jump")
+			elif velocity.y > 0:
+				animated_sprite.play("fall")
+		if is_crouching == true:
+			if velocity.y < 0:
+				animated_sprite.play("jump")
+			elif velocity.y > 0:
+				animated_sprite.play("crouch")
 		
 func toggle_flip_sprite(direction):
 	if direction == 1:
 		animated_sprite.flip_h = false
 	if direction == -1:
 		animated_sprite.flip_h = true
+
+func crouch():
+	if is_crouching:
+		return
+	is_crouching = true
+
+func stand():
+	if is_crouching == false:
+		return
+	is_crouching = false
